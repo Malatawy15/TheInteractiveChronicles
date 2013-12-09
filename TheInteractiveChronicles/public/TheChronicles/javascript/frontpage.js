5256a6a49dd2df09cd3a086ef04cfdf098772aee
Ahmad Soliman <ahmadadel922gmail.com>
2013-12-09 23:39:38 +0200

    added news feed api

--- TheInteractiveChronicles/public/TheChronicles/javascript/frontpage.js
+++ TheInteractiveChronicles/public/TheChronicles/javascript/frontpage.js
@@ -2,4 +2,16 @@ $(document).ready (function() {
 	drawImage('c1_img', 'images/Balotelli.jpg');
 	drawImage('c2_img', 'images/HarryPotter.jpg');
 	drawImage('c3_img', 'images/ACM.jpg');
+	
+	$.get('http://www.bbc.co.uk/radio1/programmes/schedules/england/today.xml',  
+		function(data) {
+			$(data).find("programme").each(function(){
+				$post = $(this).find("short_synopsis");
+				$text = $post.text();
+				if($.trim($text)!=''){	
+					$('#loadp').append("<p>"+$text+"</p>");
+				}
+			});
+		}
+	);
 });
